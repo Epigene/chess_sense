0bcd5eeaecab2a6cd2e1f2dc1d1c5072716c0808
Epigene <augusts.bautra@gmail.com>
2018-01-13 18:33:09 +0200

    Documented GIN indexing

--- app/models/chess_game.rb
+++ app/models/chess_game.rb
@@ -1,2 +1,8 @@
 class ChessGame < ApplicationRecord
+
+  # Set up GIN indexing, at least on :moveset field
+
+  # See:
+  # 1) https://niallburkley.com/blog/index-columns-for-like-in-postgres/
+  # 2) https://stackoverflow.com/questions/1566717/postgresql-like-query-performance-variations
 end
